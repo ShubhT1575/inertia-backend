c250e53f8baf2e860bbd5c99f9c6e8f0d7f6752a
shubham <shubhamnadcab@gmail.com>
2025-04-24 11:18:37 +0530

    changes

--- models/ProposalExecutedReward.js
+++ models/ProposalExecutedReward.js
@@ -4,10 +4,10 @@ const ProposalExecutedRewardSchema = new mongoose.Schema(
   {
     proposalId: { type: Number, index: true },     // Fast lookups by proposal
     user: { type: String, index: true },           // Useful for tracking rewards by user
-    reward: { type: Number },
-    timestamp: { type: Number, index: true },      // For sorting/filtering by time
+    reward: { type: Number, index: true },
+    timestamp: { type: Number},      // For sorting/filtering by time
     txHash: { type: String }, // Ensures no duplicate transactions
-    block: { type: Number, index: true },          // For queries based on block number
+    block: { type: Number },          // For queries based on block number
   },
   { timestamps: true, collection: "ProposalExecutedReward" }
 );
